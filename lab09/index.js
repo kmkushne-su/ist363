3fc4e20680c3953c11279c7bc3507b2fb335c111
kmkushne-su <kmkushne@syr.edu>
2026-03-19 15:51:27 -0400

    n/a

---
+++ lab09/index.js
@@ -0,0 +1,27 @@
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=43.0481&longitude=-76.1474&current=cloud_cover,precipitation,temperature_2m&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+
+        const temp = data.current.temperature_2m;
+        const precip = data.current.precipitation;
+        const cloud = data.current.cloud_cover;
+
+        document.getElementById("temp").textContent = "Temperature: " + temp + " °F";
+        document.getElementById("precip").textContent = "Precipitation: " + precip + " in";
+        let cloudEmoji;
+
+        if (cloud < 50) {
+          cloudEmoji = "☀️";
+        } 
+        else {
+          cloudEmoji = "☁️";
+        }
+        document.getElementById("cloud").textContent = cloudEmoji;
+      })
+      .catch(error => console.error(error));
+      
